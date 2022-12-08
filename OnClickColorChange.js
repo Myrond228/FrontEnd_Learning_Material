f24e92d09d4a5e52558621fc0b9cd1def047acf0
Myrond228 <97965545+Myrond228@users.noreply.github.com>
2022-12-08 12:20:13 +0000

    Update OnClickColorChange.js

--- OnClickColorChange.js
+++ OnClickColorChange.js
@@ -1,13 +1,18 @@
+// Store body of an HTML page in var
 const bodyTag = document.querySelector("body");
 
+// Initialize function that selects a random color from the color array
 const randomClick = function randColor(){
   const colors = ["blue", "green", "purple", "orange", "navy", "yellow", "aqua", "red"];
-    const randomize = Math.floor(Math.random() * colors.length);
-    const randomColor = colors[randomize];
+  
+  // Choose random number from 0 to length of colors array
+  const randomize = Math.floor(Math.random() * colors.length);
+  const randomColor = colors[randomize];
 
-    bodyTag.style.backgroundColor = randomColor;
+  bodyTag.style.backgroundColor = randomColor;
     
-    console.log("The user clicked and changed the color to" + randomColor)
+  console.log("The user clicked and changed the color to" + randomColor)
 }
 
+// Call function everytime webpage is clicked
 bodyTag.onclick = randomClick;
